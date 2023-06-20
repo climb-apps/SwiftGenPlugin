90cf4a70907f1ceecc6da13ec2e599f709bcab41
Ihor Teltov <igor.teltov@gmail.com>
2023-06-20 17:06:02 +0300

    added second config location

--- Plugins/SwiftGenPlugin/Plugin.swift
+++ Plugins/SwiftGenPlugin/Plugin.swift
@@ -39,7 +39,7 @@ extension SwiftGenPlugin: XcodeBuildToolPlugin {
     let fileManager = FileManager.default
 
     // Possible paths where there may be a config file (root of package, target dir.)
-    let configurations: [Path] = [context.xcodeProject.directory]
+    let configurations: [Path] = [context.xcodeProject.directory, context.xcodeProject.directory.appending(target.displayName)]
       .map { $0.appending("swiftgen.yml") }
       .filter { fileManager.fileExists(atPath: $0.string) }
 
